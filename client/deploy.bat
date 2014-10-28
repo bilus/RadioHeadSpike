bd2928f469df527bd9f7093eccbb04c50dd5544d
xnitchlooms <xnitchlooms@users.noreply.github.com>
2014-10-28 16:26:15 +0100

    Make it deploy on Windows (not fully tested).

---
+++ client/deploy.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+set TARGET_SERIAL_PORT=%1
+set CLIENT_ADDRESS=%2
+REM Force rebuild because CLIENT_ADDRESS may have changed.
+touch client.cpp 
+cd build
+make && make client-upload && cd .. 
+rem && ./monitor.bat %TARGET_SERIAL_PORT%
+echo Deployed to %TARGET_SERIAL_PORT%
